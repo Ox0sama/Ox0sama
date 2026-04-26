0b4f4f51e64e0f78c4c765c56eee7b2dc24d8110
Ox0sama <osama.ibrahim.gomaa@gmail.com>
2026-04-26 10:39:05 -0400

    commit & push navigation.py

---
+++ navigation.py
@@ -0,0 +1,18 @@
+ROLL_CH = 1
+PITCH_CH = 2
+THROTTLE_CH = 3
+YAW_CH = 4
+
+# PWM values
+PWM_MIN = 1000
+PWM_MID = 1500
+PWM_MAX = 2000
+
+# Update rate
+SEND_HZ = 20
+SEND_INTERVAL = 1.0 / SEND_HZ
+
+# Deadzone for sticks
+DEADZONE = 0.1
+
+# ------
